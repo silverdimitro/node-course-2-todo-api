c6e57e5dadcbda2a54ca48737948b3370205567d
silverdimitro <dumdum.dd9@gmail.com>
2017-03-08 18:38:57 +0530

    added get /todo test case

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ app.post('/todos',(req,res)=>{
     res.status(400).send(e);
   })
 });
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{res.status(400).send(e)});
+
+});
+
 app.listen(3000,()=>{
   console.log("server started at 3000");
 })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 var {app} =  require('./../server');
 var {Todo}=require('./../models/todo');
 
+const todos= [{
+  text:'first test todo'
+},{
+
+  text:'second test todo'
+}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe('post /todos',()=>{
@@ -24,7 +33,7 @@ describe('post /todos',()=>{
           return done(err);
         }
 
-        Todo.find().then((todos)=>{
+        Todo.find({text}).then((todos)=>{
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -45,7 +54,7 @@ describe('post /todos',()=>{
           }
 
           Todo.find().then((todos)=>{
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           }).catch((e)=>done(e));
 
@@ -53,4 +62,16 @@ describe('post /todos',()=>{
 
   });
 
+});
+
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
