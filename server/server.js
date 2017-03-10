0f8cda7670d0b4c27ab20f2deb41897b1841e625
silverdimitro <dumdum.dd9@gmail.com>
2017-03-10 20:30:45 +0530

    mongo added for web

--- server/server.js
+++ server/server.js
@@ -30,6 +30,29 @@ app.post('/users',(req,res)=>{
     res.status(400).send(e);
   })
 });
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id)){
+    return  res.status(404).send('not a valid id')
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt= new Date().getTime();
+  }else {
+    body.completed=false;
+    body.completedAt=null;
+  }
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+});
 
 app.get('/',(req,res)=>{
   res.send('this is a root');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -141,3 +141,41 @@ describe('DELETE /todos/:id',()=>{
       .end(done);
 });
 });
+describe('PATCH /todos/:id',()=>{
+  it('should UPDATE  a todo',(done)=>{
+    var hexId=todos[0]._id.toHexString();
+    var text="this should be a text";
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({completed:true,text})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done );
+  });
+  it('should clear completedAt ',(done)=>{
+    var hexId=todos[1]._id.toHexString();
+    var text="this should be a text!!";
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({completed:false,text})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done );
+  });
+//   it('should return 404 for non-object ids',(done)=>{
+//     request(app)
+//       .delete('/todos/123abd')
+//       .expect(404)
+//       .end(done);
+// });
+});
