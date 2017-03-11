8b196a3a73cf17bc5874e67749098fe925d06755
silverdimitro <dumdum.dd9@gmail.com>
2017-03-11 21:13:48 +0530

    mlab working

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,19 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-  id:10
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc1');
-console.log("deocded",decoded);
+
+
+
+
+
+
+
+// var data = {
+//   id:10
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc1');
+// console.log("deocded",decoded);
 // var message = "hi";
 // var hash = SHA256(message).toString();
 // console.log(`message ${message} \nhash : ${hash}`);
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const mongoose=require('mongoose');
 const validator = require('validator');
+const bcrypt= require('bcryptjs');
 var UserSchema = new mongoose.Schema({
   email:{
     type:String,
@@ -62,5 +63,18 @@ UserSchema.statics.findbyToken=function (token) {
     'tokens.access':'auth'
   });
 };
+UserSchema.pre('save',function (next) {
+  var user = this;
+  if (  user.isModified('password')) {
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password=hash;
+        next();
+      });
+    });
+  }else {
+    next();
+  }
+});
 var User=mongoose.model('User',UserSchema);
 module.exports ={User};
